42d8515dbe94630f64c078e25b6b5ee665f69dda
Brian Crookshanks <zeron30@hotmail.com>
2024-01-03 09:09:26 -0700

    Repo init

---
+++ app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>
+        Unfortunately, we could not find the requested meal. Please check your
+        spelling and/or formatting.
+      </p>
+    </main>
+  );
+}
